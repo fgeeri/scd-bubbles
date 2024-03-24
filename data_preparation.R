449e34d449ab0b2d0f695c62c601e0d1517c85e0
fgeeri <102463666+fgeeri@users.noreply.github.com>
2024-03-24 21:28:26 +0100

    Add duration of proceedings to data preparation

--- data_preparation.R
+++ data_preparation.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(jsonlite)
 
-# d_bger <- read_rds(file = "")  # File path needs to be defined!
+d_bger <- read_rds(file = "bger-2023-5.rds")  # File path needs to be defined!
 
-json_bger <- d_bger %>% select(docref, date, outcome, length, leading_case, app_represented, area_general) %>% 
+json_bger <- d_bger %>% select(docref, date, outcome, length, leading_case, app_represented, area_general, proc_duration) %>% 
   mutate(leading_case = if_else(is.na(leading_case), FALSE, TRUE),
          area_general = fct_recode(area_general, o = "Öffentliches Recht",
                                        p = "Privatrecht", s = "Strafrecht"),
@@ -11,12 +11,17 @@ json_bger <- d_bger %>% select(docref, date, outcome, length, leading_case, app_
                               p = "partly granted",
                               i = "inadmissible",
                               r = "rejected",
-                              w = "writeoff")) %>% 
+                              w = "writeoff"),
+         duration_raw = round(log(proc_duration) * 1000, 0),
+         duration = if_else(duration_raw < 0, 0, duration_raw)) %>% 
   filter(is.na(area_general) == FALSE) %>% 
   filter(is.na(app_represented) == FALSE) %>% 
   filter(is.na(outcome) == FALSE) %>% 
-  select(docref, date, length, outcome, leading_case, app_represented, area_general) %>% 
+  filter(is.na(duration) == FALSE) %>% 
+  select(docref, date, length, outcome, leading_case, app_represented, area_general, duration) %>% 
   rename(size = "length", colour = "outcome")
 
 exportJSON <- toJSON(json_bger)
 write(exportJSON, "bubbles.json")
+
+# summary(json_bger$duration)  # Mean is 4800
