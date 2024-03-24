c2f4f4d70b4955ff6f1a96f0a261e27c24510bd2
meinj1 <jasper.meinert@students.bfh.ch>
2024-03-24 20:16:51 +0100

    adding date counter

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ async function fetchData() {
     try {
         const response = await fetch('bubbles.json');
         const data = await response.json();
+
+        // Sort the data by date
+        data.sort((a, b) => {
+            // Assuming each item in the data has a 'date' property
+            const dateA = new Date(a.date);
+            const dateB = new Date(b.date);
+            return dateA - dateB;
+        });
+
         return data;
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -12,14 +21,18 @@ async function fetchData() {
 }
 
 const loadBatch = 1000
+let currentDate="";
 function createDots(data) {
     const visualization = document.querySelector('.visualization');
+    
+    //Set the start date
+    currentDate=data[0].date;
+    updateDateIndicator(data[0].date);
 
     // Loop through the data and create a dot for each entry
     for (let i = 0; i < loadBatch; i++) {
         const entry = data[i];
         const dot = document.createElement('div');
-        console.log(entry)
         dot.classList.add('dot');
         dot.style.backgroundColor = getColor(entry.colour); // Set dot color
         dot.style.width = (entry.size / 200) + 'px'; // Set dot size
@@ -30,7 +43,7 @@ function createDots(data) {
         visualization.appendChild(dot);
 
         // Animate the dot
-        animateDot(dot, i);
+        animateDot(dot, i, entry);
         // If it's the last dot, remove all dots and load the next batch after 1000 seconds
         if (i === (loadBatch-1) || !data[i + 1]) {
             setTimeout(async () => {
@@ -51,12 +64,12 @@ function removeDots(visualization) {
 }
 
 // Function to animate a dot
-function animateDot(dot, index) {
+function animateDot(dot, index, entry) {
 
     const animationDuration = 15000 + Math.random()*5000; // Animation duration in milliseconds
 
     // Start the animation
-    dot.animate(
+    let animation = dot.animate(
         [
             { top: '100%', opacity: 0.8 }, // From bottom with opacity
             { top: '-200px', opacity: 0.8 } // To top with opacity
@@ -68,6 +81,20 @@ function animateDot(dot, index) {
             delay: index * 1000 // Delay based on index
         }
     );
+            // Call a function when the animation starts
+    animation.onfinish = function () {
+        // Call your function here, animation has finished
+        if (new Date(entry.date) > new Date(currentDate)) {
+            currentDate = entry.date;
+            updateDateIndicator(entry.date);
+        }
+    };
+}
+
+// Function to update the date indicator
+function updateDateIndicator(date) {
+    const dateIndicator = document.querySelector('.date-indicator');
+    dateIndicator.textContent = date;
 }
 
 // Function to get color based on colour code
